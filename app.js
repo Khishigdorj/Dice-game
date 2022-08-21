60e3482677f685e5695c49e92213cd4fd8af4e12
HeavenHero <khishigt0308@gmail.com>
2022-08-21 15:38:18 +0800

    New game towchiig nemne.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // Lesson 41:
-/* Тоглогчийн ээлжийг хадгалах хувьсагч: 
+// Хувьсагчдийг оноож, тоглоом эхлэхэд бэлдэх нь.
+
+/* Тоглож байгаа тоглогчийн ээлжийг хадгалах хувьсагч: 
 Нэгдүгээр тоглогчийг 0,
 Хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
 */
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчийн цуглуулсан оноог хадгалах хувьсагч:
 // Array ашиглан хийх нь.
@@ -53,6 +55,8 @@ document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 
 // Lesson 42:
+// Шоо хаях товчыг бэлдэх нь.
+
 // 1. Уртаар
 // Roll dice товчыг Дом - оос аваад click Эвент холбоно.
 document.querySelector('.btn-roll').addEventListener('click', shooShid);
@@ -84,6 +88,109 @@ document
     var diceNumber = Math.floor(Math.random() * 6) + 1;
     diceDom.style.display = 'block';
     diceDom.src = 'dice-' + diceNumber + '.png';
+
+    // Lesson - 43:
+    // Оноог хадгалж, тоглогчийн ээлжийг солих нь.
+
+    // Буусан тоо нь 1 ээс ялгаатай бол оноог нь цааш хадгална.
+    if (diceNumber !== 1) {
+      // 1 ээс бусад тоо буусан тохиолдолд уг тоог тоглогчид нэмж өгнө.
+      // roundScore = Ээлжийн оноог түр цуглуулагч хувьсагч
+      roundScore = roundScore + diceNumber;
+      document.getElementById('current-' + activePlayer).textContent =
+        roundScore;
+    } /* Хэрвээ 1 буучих юм бол идэвхтэй тоглогч 0 байвал 1 болго.
+    Харин тоглогч 1 байвал 0 болго. Ингэснээр тоглогч солигдно.
+    */ else {
+      // 1 буусан учраас энэ тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгоно.
+      roundScore = 0;
+      // 1 буусан учраас энэ тоглогчийн ээлжиндээ цуглуулсан оноог textContent дээр 0 болгоно.
+      document.getElementById('current-' + activePlayer).textContent = 0;
+
+      // Тэгээд тоглогчийн ээлжийг нөгөө тоглуулагч руу шилжүүлнэ.
+      // if (activePlayer === 0) {
+      //   activePlayer = 1;
+      // } else {
+      //   activePlayer = 0;
+      // }
+
+      // Гурвалсан оператороор товчхон бичих нь :
+      // Хэрвээ 0 дугаар тоглогч бол activePlayer ээ 1 болгоно.
+      // Харин 1 - р тоглогч бол activePlayer ээ 0 болгоно.
+      activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+      // Улаан цэгийг шилжүүлнэ.
+      document.querySelector('.player-0-panel').classList.toggle('active');
+      document.querySelector('.player-1-panel').classList.toggle('active');
+      // toggle нь уг класст active гэх таг байвал устгана, байхгүй бол нэмнэ.
+
+      // document.querySelector('.player-0-panel').classList.remove('active');
+      //.player-0-panel гэх тагны active class - ийг хасаж өгнө.
+      // document.querySelector('.player-1-panel').classList.add('active');
+      //.player-0-panel гэх тагны active class - ийг нэмэх нь.
+
+      // Тоглогч солигдох явцад шоог түр алга болгоно.
+      diceDom.style.display = 'none';
+    }
   });
 
-// Lesson - 43:
+// Lesson 44 : Hold(btn-hold) товчыг ажиллагаанд оруулах нь.
+// 1. Товчыг дарах үед яахыг зааж өгнө.
+document.querySelector('.btn-hold').addEventListener('click', function () {
+  // Тоглож буй хүний цуглуулсан ээлжний оноог глобал оноон дээр нь нэмж өгнө.
+  // scores[0] гэдэг нь 1-р тоглогч. Харин [1] нь 2-р тоглогч юм.
+  // if (activePlayer === 0) {
+  //   scores[0] = score[0] + roundScore;
+  // } else {
+  //   scores[1] = score[1] + roundScore;
+  // }
+  scores[activePlayer] = scores[activePlayer] + roundScore;
+
+  // Харин одоо дэлгэц дээрх тоог өөрчлөнө.
+  document.getElementById('score-' + activePlayer).textContent =
+    scores[activePlayer];
+
+  // Уг тоглогчийн оноо 100 - аас их эсэхийг шалгана.
+  // Тэгээд 100 - аас их байвал Winner.
+  // Мөн доорх үйлдлүүд рүү дамжихгүй.
+  if (scores[activePlayer] >= 20) {
+    document.getElementById('name-' + activePlayer).textContent = 'ЯЛАГЧ !!!';
+    document
+      .querySelector('.player-' + activePlayer + '-panel')
+      .classList.add('winner');
+    // .player-1-panel
+    document
+      .querySelector('.player-' + activePlayer + '-panel')
+      .classList.remove('active');
+    // active буюу улаан бөөрөнхийг хасах нь.
+  } else {
+    // Тоглогчийн ээлжийг солино.
+    // Сольж өгдөг функцаа дуудаж өгөв.
+    switchToNextPlayer();
+  }
+});
+
+// Тоглогчоо шилжүүлдэг функц бичих нь:
+function switchToNextPlayer() {
+  // Ээлжийн оноог нь дээш шилжүүлсэн учраас буцаагаад 0 болгоно.
+  roundScore = 0;
+  document.getElementById('current-' + activePlayer).textContent = '0';
+
+  // Тоглогчийн ээлжийг сольно.
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+
+  // Улаан цэгийг шилжүүлнэ.
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  // toggle нь уг класст active гэх таг байвал устгана, байхгүй бол нэмнэ.
+
+  // Шоог түр алга болно.
+  diceDom.style.display = 'none';
+}
+
+// Lesson 45: New Game Button
+document.querySelector('.btn-new').addEventListener('click', function () {});
